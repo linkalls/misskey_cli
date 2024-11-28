b764d34783e3aba2e5542212be11976dfbf92a3a
poteto <naonyan1135@gmail.com>
2024-11-28 19:31:22 +0900

    .gitignoreを更新し、dotenvパッケージをenv.g.dartに置き換え、misskey_on_cli.dartで環境変数を使用するように変更。pubspec.yamlからdotenvの依存関係を削除し、新しいenvenパッケージを追加。

--- lib/misskey_on_cli.dart
+++ lib/misskey_on_cli.dart
@@ -3,21 +3,23 @@ library;
 export 'src/misskey_on_cli_base.dart';
 
 import "package:dio/dio.dart";
-import 'package:dotenv/dotenv.dart';
+// import 'package:dotenv/dotenv.dart';
+import 'package:misskey_on_cli/gen/env.g.dart';
 import "dart:io";
 
 void main(List<String> arguments) async {
   try {
-    final env = DotEnv(includePlatformEnvironment: true)..load();
+    final env = Env.token;
+    final server = Env.server;
     final dio = Dio();
     if (arguments.length == 1) {
-      await dio.post("https://misskey.io/api/notes/create",
+      await dio.post("${server}/api/notes/create",
           data: {
             "text": arguments[0],
           },
           options: Options(headers: {
             //* 全部がdataにかけるわけじゃないので注意
-            "Authorization": "Bearer ${env["Token"]}",
+            "Authorization": "Bearer ${env}",
             "Content-Type": "application/json",
           }));
       print("Posted! ${arguments[0]}");
